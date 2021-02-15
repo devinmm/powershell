c3d3e73a6211548f05cd614428746ce98b8eb497
Devin Mulrooney <devin.mm@gmail.com>
2021-02-14 23:37:32 -0700

    Updated: Script will always run 10min sleep.

--- vmware_tools_status.ps1
+++ vmware_tools_status.ps1
@@ -1,13 +1,16 @@
 $ServiceName = 'VMTools'
 $arrService = Get-Service -Name $ServiceName
 
-while ($arrService.Status -ne 'Running')
+while ($true)
 {
 
-    Start-Service $ServiceName
-    write-host $arrService.status
-    write-host 'Service starting'
-    Start-Sleep -seconds 60
+    if ($arrService.Status -ne 'Running'){
+        Start-Service $ServiceName
+        write-host $arrService.status
+        write-host 'Service starting'
+    }
+    
+    Start-Sleep -seconds 600
     $arrService.Refresh()
     if ($arrService.Status -eq 'Running')
     {
